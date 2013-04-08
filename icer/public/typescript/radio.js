381399b0e759c500a5207b622dcabde0804346b0
Catalin Dumitru <colin.dumitru@gmail.com>
2013-04-08 21:28:12 +0300

    radio WIP

--- icer/public/typescript/radio.js
+++ icer/public/typescript/radio.js
@@ -3,12 +3,58 @@ var RadioBinder = (function () {
     }
 
     RadioBinder.prototype.buildPage = function (rootNode) {
-        this.rootNode = rootNode;
+        this.radioManager = new RadioManager(rootNode);
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioCustomCriteria", true, function () {
+            return $("#customCriteriaInput").val();
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioRecentSongsCriteria", false, function () {
+            return "Recent Songs";
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioRecentGenresCriteria", false, function () {
+            return "Recent Genres";
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioRecentAlbumsCriteria", false, function () {
+            return "Recent Albums";
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioPastConcertsCriteria", false, function () {
+            return "Past Concerts";
+        }));
+        this.radioManager.bind();
     };
     RadioBinder.prototype.bind = function () {
     };
     RadioBinder.prototype.unbind = function () {
     };
     return RadioBinder;
+})();
+var RadioManager = (function () {
+    function RadioManager(rootNode) {
+        this.criterias = [];
+    }
+
+    RadioManager.prototype.addCriteria = function (criteria) {
+        this.criterias.push(criteria);
+    };
+    RadioManager.prototype.bind = function () {
+        var _this = this;
+        $("#radioAddButtonCell").click(function () {
+            _this.toggleCriteriasInput();
+        });
+    };
+    RadioManager.prototype.toggleCriteriasInput = function () {
+        this.criterias.forEach(function (criteria) {
+            $("#" + criteria.id).toggle(300);
+        });
+    };
+    return RadioManager;
+})();
+var RadioCriteriaInput = (function () {
+    function RadioCriteriaInput(id, repeatable, labelFormatter) {
+        this.id = id;
+        this.repeatable = repeatable;
+        this.labelFormatter = labelFormatter;
+    }
+
+    return RadioCriteriaInput;
 })();
 //@ sourceMappingURL=radio.js.map
--- icer/public/typescript/radio.ts
+++ icer/public/typescript/radio.ts
@@ -1,8 +1,26 @@
 class RadioBinder implements SectionBinder {
-    private rootNode:any;
+    private radioManager:RadioManager;
 
     buildPage(rootNode:any) {
-        this.rootNode = rootNode;
+        this.radioManager = new RadioManager(rootNode);
+
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioCustomCriteria", true, () => {
+            return $("#customCriteriaInput").val();
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioRecentSongsCriteria", false, () => {
+            return "Recent Songs";
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioRecentGenresCriteria", false, () => {
+            return "Recent Genres";
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioRecentAlbumsCriteria", false, () => {
+            return "Recent Albums";
+        }));
+        this.radioManager.addCriteria(new RadioCriteriaInput("radioPastConcertsCriteria", false, () => {
+            return "Past Concerts";
+        }));
+
+        this.radioManager.bind();
     }
 
     bind() {
@@ -11,5 +29,36 @@ class RadioBinder implements SectionBinder {
     unbind() {
 
     }
+}
 
+class RadioManager {
+    private criterias:RadioCriteriaInput[] = [];
+
+    constructor(rootNode:any) {
+
+    }
+
+    addCriteria(criteria:RadioCriteriaInput) {
+        this.criterias.push(criteria);
+    }
+
+    bind() {
+        $("#radioAddButtonCell").click(() => {
+            this.toggleCriteriasInput();
+        })
+
+    }
+
+    private toggleCriteriasInput() {
+        this.criterias.forEach((criteria) => {
+            $("#" + criteria.id).toggle(300);
+        });
+    }
+
+}
+
+class RadioCriteriaInput {
+    constructor(public id:string, public repeatable:bool, public labelFormatter:() => string) {
+
+    }
 }
