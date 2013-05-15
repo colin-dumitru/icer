5324f7afd3651305ef74b338679de538b0ffde37
Irina <irinanaum.ni@gmail.com>
2013-05-15 16:45:15 +0300

    Playlist module - copying delete playlist problem resolved

--- app/controllers/playlist/Playlists.scala
+++ app/controllers/playlist/Playlists.scala
@@ -82,9 +82,12 @@ object Playlists extends Controller {
 
   def copyPlaylist(idPlaylist: String) = Secured {
     (request, userId) => {
-      val newPlaylist = new Playlist(null, userId, Playlist.getNameForPlaylist(idPlaylist.toLong));
-      val id = Playlist.create(newPlaylist);
-      Song.getSongsForPlaylist(idPlaylist.toLong) foreach (song => Playlist.addSongToPlaylist(id.get, song.mbid, userId));
+      val playlistName = Playlist.getNameForPlaylist(idPlaylist.toLong);
+      if (!playlistName.equals("")) {
+        val newPlaylist = new Playlist(null, userId, playlistName);
+        val id = Playlist.create(newPlaylist);
+        Song.getSongsForPlaylist(idPlaylist.toLong) foreach (song => Playlist.addSongToPlaylist(id.get, song.mbid, userId));
+      }
       Redirect(controllers.routes.Application.index().url)
     }
   }
